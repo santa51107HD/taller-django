ef37cb42ef0ff0ee817fe1ba41e878b973635cb5
Juan Camilo Santa Gomez <santa51107@gmail.com>
2023-09-29 20:51:38 -0500

    model admin

--- core/admin.py
+++ core/admin.py
@@ -5,12 +5,45 @@ from django.contrib import admin
 from .models import *
 
 
-class MyAdminView(admin.ModelAdmin):
+class UnivallunoForm(ModelForm):
+    class Meta:
+        fields = [
+            "nombre",
+            "apellidos",
+            "tipoDeUnivalluna",
+            "tipoDeDocumento",
+            "numeroDeDocumento",
+            "codigoDeEstudiante",
+            "email",
+            "is_active",
+            "disponible",
+        ]
+        model= Univalluno
+
+    def clean(self):
+        intancia = self.instance
+
+        print(self.cleaned_data)
+
+        if(self.instance.numeroDeDocumento != self.cleaned_data["numeroDeDocumento"]):
+            pass
+            print("si cambia")
+            raise forms.ValidationError("No se puede modificar el campo numero de documento")
+        if(self.instance.tipoDeDocumento != self.cleaned_data["tipoDeDocumento"]):
+            pass 
+            print("si cambia")
+            raise forms.ValidationError("No se puede modificar el campo tipo de documento")
+        
+        super().clean()
+
+class UnivallunoAdminView(admin.ModelAdmin):
+    form = UnivallunoForm
     def save_model(self, request, obj, form, change):
-        print("creado")
         super().save_model(request, obj, form, change)
 
-admin.site.register(Univalluno,MyAdminView)
+
+
+admin.site.register(Univalluno, UnivallunoAdminView)
 admin.site.register(ArticuloDeportivo)
 admin.site.register(Prestamos)
 admin.site.register(Multas)

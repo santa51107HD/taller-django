50fec72c2150768facbe4caa061f58aab81f36e8
Juan Camilo Santa Gomez <santa51107@gmail.com>
2023-09-30 13:56:54 -0500

    generar y pagar multa

--- core/views.py
+++ core/views.py
@@ -7,14 +7,14 @@ from .models import Multas, Prestamos
 import datetime
 # Create your views here.
 
-#todos los prestamos que son menores o iguales a hoy y no esten pagados
+#todos los prestamos que su fecha de pago es menor o igual a hoy y no esten pagados
 @api_view(['GET'])
 def generarMulta(request):
 
     
     prestamos_vencidos = Prestamos.objects.filter(fecha_vencimiento_prestamos__lte=datetime.date.today()).filter(pagado = False)
 
-
+    # si tenemos algun prestamo vencido se le genera una multa al univalluno
     if prestamos_vencidos:
 
         for prestamo in prestamos_vencidos:
@@ -33,16 +33,30 @@ def generarMulta(request):
     else:
         return Response({"error": False, "mensaje": "No se generaron multas"}, status=status.HTTP_200_OK)
     
-
+#metodo para pagar una multa
 @api_view(['POST'])
-def generarMulta(request, pk):
+def pagarMulta(request):
 
-    multa = Multas.objects.get(pk=pk)
-    if multa.pagado == False:
-        multa.pagado= True
-        multa.save()
-# estaba haciendo lo de pagar
-
-    
-    
+    multa_pagada = Multas.objects.get(pk=request.data["id_multa"])
+    if multa_pagada.pagado == False:
+        multa_pagada.pagado= True
+        multa_pagada.fecha_pago = datetime.date.today()
+        multa_pagada.save()
+        univalluno = multa_pagada.univalluno
+        multas = Multas.objects.filter(univalluno=univalluno)
+        #revisar si el univalluno tiene alguna multa sin pagar
+        for multa in multas:
+            if multa.pagado == False:
+                return Response({"error":False, "mensaje": "Se ha pagado la multa"}, status=status.HTTP_200_OK)
+        #liberar a los articulos deportivos 
+        for multa in multas:
+            multa.articuloDeportivo.disponible= True
+            multa.save()
         
+        #liberar al univalluno que pago sus multas
+        univalluno.disponible=True
+        univalluno.save()    
+        
+        return Response({"error":False, "mensaje": "Se ha pagado la multa y se liberaron los articulos y el estudiante"}, status=status.HTTP_200_OK)
+    else:
+        return Response({"error":False, "mensaje": "La multa ya esta pagada"}, status=status.HTTP_200_OK)
--- univalle_esport/urls.py
+++ univalle_esport/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from core import views
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('core/multa', views.generarMulta, name='multa'),
+    path('core/multa/pagar/', views.pagarMulta, name='multa'),
 ]
